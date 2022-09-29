1632ecaa54ddc0d50211fc07091f7a89653c5404
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-09-29 17:37:04 +0100

    updated

---
+++ react1/src/Home.js
@@ -0,0 +1,12 @@
+import React   from 'react'
+import './Home.css'
+
+export default function Home() {
+
+   alert('hello')
+    return (
+        <div className='homeWrapper'>
+        </div>  
+
+    )
+}
---
+++ react1/src/WelcomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './WelcomePage.css'
+export default function WelcomePage() {
+    
+  
+    return (
+        <div>
+        welcome
+        </div>
+    )
+}
